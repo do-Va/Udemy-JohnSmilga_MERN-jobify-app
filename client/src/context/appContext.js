ab20c169b761441c8907ef70cf47a878186b9a6f
do-Va <varisdogukan@outlook.com>
2022-07-17 13:32:15 +0300

    Created basic Reducer

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,7 @@
 import { useState, useReducer, useContext, createContext } from 'react';
 
+import reducer from './reducer';
+
 const initialState = {
   isLoading: false,
   showAlert: false,
@@ -10,7 +12,7 @@ const initialState = {
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
-  const [state, setState] = useState(initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
---
+++ client/src/context/reducer.js
@@ -0,0 +1,5 @@
+const reducer = (state, action) => {
+  throw new Error(`no sch action : ${action.type}`);
+};
+
+export default reducer;
