e8b423e8e911f446a2ebfe86ae9095b6906d9170
do-Va <varisdogukan@outlook.com>
2022-07-17 11:31:38 +0300

    Updated Register page and added FormRow component to in it.

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,70 @@
+import { useState, useEffect } from 'react';
+import { Logo, FormRow } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  // TODO global state and useNavigate
+
+  // #region Methods
+  const handleChange = e => {
+    console.log(e.target);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    console.log(e.target);
+  };
+  // #endregion
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {
+          // #region FormRow
+        }
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type="text"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        {
+          // #endregion
+        }
+
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
