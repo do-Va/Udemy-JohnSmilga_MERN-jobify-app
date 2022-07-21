88693060160f43591abd638bc3cac7b47c9b91fe
do-Va <varisdogukan@outlook.com>
2022-07-21 19:03:15 +0300

    Auth Controller and Route Structure

--- server.js
+++ server.js
@@ -1,22 +1,42 @@
 import express from 'express';
 const app = express();
+
 import dotenv from 'dotenv';
 dotenv.config();
+
+// db and authenticateUser
+import connectDB from './db/connect.js';
+
+// router
+import authRouter from './routes/authRoutes.js';
 
 // middleware
 import notfoundMiddleWare from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  throw new Error('error');
   res.send('Wellcome!');
 });
+
+app.use('/api/v1/auth', authRouter);
 
 app.use(notfoundMiddleWare);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
