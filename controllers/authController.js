5a0cc901cb1014f956a7e416ce056917486eb68a
do-Va <varisdogukan@outlook.com>
2022-07-24 08:33:45 +0300

    Added CustomApıError

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,23 @@
+import { StatusCodes } from 'http-status-codes';
 import User from '../models/User.js';
+import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { name, email, password } = req.body;
 
-  res.status(201).json({ user });
+  if (!name || !email || !password) {
+    throw new BadRequestError('please provide all values');
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  const user = await User.create({ name, email, password });
+
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
 const login = async (req, res) => {
